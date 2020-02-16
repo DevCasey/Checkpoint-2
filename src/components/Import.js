6857f8544af669a6594e78c52701dd5de67336d7
Casey Calkins <c.angelcalkins@outlook.com>
2020-02-16 12:00:47 -0600

    Changing PCs

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Button, Container, TableContainer, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
+import '../App.css'
+
+
+
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+        <div>
+            <Container maxWith="xl" className="button-container">
+                <Button variant="contained" color="primary">Import</Button>
+            </Container>
+
+            <Container className="main-container">  
+                <Container className="table-container" >
+                    <TableBody className="table-body">
+                        <TableCell align="left">ID</TableCell>
+                        <TableCell align="left">Make</TableCell>
+                        <TableCell align="left">Actions</TableCell>
+                    </TableBody>
+                </Container>  
+            </Container>
+        </div>
     )
 }
 
